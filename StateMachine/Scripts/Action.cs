8ba53af3efdb11d8e0fe00dd2c0b28b8de069dea
skulllism <editer77@gmail.com>
2018-05-31 08:37:39 +0900

    implements

---
+++ StateMachine/Scripts/Action.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Advanced
+{
+    public abstract class Action : ScriptableObject
+    {
+        public abstract void OnAction();
+    }
+}
