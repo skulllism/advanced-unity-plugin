e20ac05cde30f9f40e5866bef14b78fca71021d4
skulllism <editer77@gmail.com>
2018-05-31 21:05:17 +0900

    arrange folder

---
+++ StateMachine/Scripts/ScriptableObject/Action.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Advanced
+{
+    namespace ScriptableObject
+    {
+
+        /*
+    * @brief this is a Action that invoke on state
+    * @author Kay
+    * @date 2018-05-31
+    * @version 0.0.1
+    * */
+        public abstract class Action : PrototypeScriptableObject
+        {
+            public abstract void OnAction(ScriptableGameobject actor);
+        }
+    }
+}
---
+++ StateMachine/Scripts/ScriptableObject/Transition.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Advanced
+{
+    namespace ScriptableObject
+    {
+        /*
+    * @brief this is a logic that states can transition
+    * @details you can assemble Decision on slot. state would be transit by your decision
+    * @author Kay
+    * @date 2018-05-31
+    * @version 0.0.1
+    * */
+        [CreateAssetMenu(menuName = "AdvancedUnityPlugin/Transition")]
+        public class Transition : PrototypeScriptableObject
+        {
+            [Header("Decision")]
+            public Decision[] decisions;
+
+            [Header("Target State ID")]
+            public string stateID;
+
+            private Decision[] clones;
+
+            public bool CanBeTransit(ScriptableGameobject obj, out string stateID)
+            {
+                stateID = this.stateID;
+
+                for (int i = 0; i < decisions.Length; i++)
+                {
+                    if (decisions[i].Decide(obj) != decisions[i].isTrue)
+                        return false;
+                }
+
+                return true;
+            }
+
+            public override PrototypeScriptableObject Clone()
+            {
+                return Instantiate(this);
+            }
+
+            public override void Init(ScriptableGameobject obj)
+            {
+                clones = SetClones(obj, decisions);
+            }
+        }
+    }
+}
+
