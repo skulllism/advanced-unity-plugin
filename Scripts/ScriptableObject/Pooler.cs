22f88c6c7b634647809a34ceee35f3648fddaeb2
skulllism <editer77@gmail.com>
2018-06-07 05:06:46 +0900

    implement pooler

---
+++ Scripts/ScriptableObject/Pooler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace AdvancedUnityPlugin
+{
+    [CreateAssetMenu]
+    public class Pooler : ScriptableObject
+    {
+        [Serializable]
+        public class Pool
+        {
+            [CreateAssetMenu]
+            public class Data : ScriptableObject
+            {
+                public string originName;
+                public int count;
+            }
+
+            public string key;
+
+            public Data[] datas;
+        }
+
+        public GameObjectContainer container;
+        public Pool[] datas;
+
+        private Pool GetData(string key)
+        {
+            foreach (var data in datas)
+            {
+                if (data.key == key)
+                    return data;
+            }
+
+            Debug.Log("[Pooler] Not Found Data : " + key);
+            return null;
+        }
+
+        public IEnumerator Pooling(string key, System.Action onStart, System.Action onFinish)
+        {
+            if (onStart != null)
+                onStart();
+
+            yield return null;
+
+            Pool data = GetData(key);
+
+            foreach (var poolData in data.datas)
+            {
+                container.CreatePool(poolData.originName, poolData.count);
+                yield return null;
+            }
+
+            if(onFinish !=null)
+                onFinish();
+        }
+    }
+}
