e215962ecb988535434cbaebbe5531354d97e2b5
skulllism <editer77@gmail.com>
2018-06-07 03:06:06 +0900

    refactor game-event

--- Scripts/ScriptableObject/GameEvent.cs
+++ Scripts/ScriptableObject/GameEvent.cs
@@ -6,60 +6,28 @@ namespace AdvancedUnityPlugin
     [CreateAssetMenu]
     public class GameEvent : ScriptableObject
     {
-        public interface GameEventListener
-        {
-            void OnRegister(Listener listener);
-            void OnUnregister(Listener listener);
-            void OnRaise();
-        }
-
         public interface Listener
         {
             void OnEventRaised();
         }
 
-        private List<GameEventListener> gameEventListeners = new List<GameEventListener>();
         private List<Listener> eventListeners = new List<Listener>();
 
-        public void Raise()
+        public virtual void Raise()
         {
-            for (int i = 0; i < gameEventListeners.Count; i++)
-            {
-                gameEventListeners[i].OnRaise();
-            }
             for (int i = 0; i < eventListeners.Count; i++)
             {
                 eventListeners[i].OnEventRaised();
             }
         }
 
-        public void RegisterGameEventListener(GameEventListener listener)
+        public virtual void RegisterListener(Listener listener)
         {
-            gameEventListeners.Add(listener);
-        }
-
-        public void UnregisterGameEventListener(GameEventListener listener)
-        {
-            gameEventListeners.Remove(listener);
-        }
-
-        public void RegisterListener(Listener listener)
-        {
-            for (int i = 0; i < gameEventListeners.Count; i++)
-            {
-                gameEventListeners[i].OnRegister(listener);
-            }
-
             eventListeners.Add(listener);
         }
 
-        public void UnregisterListener(Listener listener)
+        public virtual void UnregisterListener(Listener listener)
         {
-            for (int i = 0; i < gameEventListeners.Count; i++)
-            {
-                gameEventListeners[i].OnUnregister(listener);
-            }
-
             eventListeners.Remove(listener);
         }
     }
