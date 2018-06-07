e14111a3dfcdf89c981e959288abc5822d1d4a63
skulllism <editer77@gmail.com>
2018-06-07 21:47:44 +0900

    add updater

---
+++ Scripts/ScriptableObject/Updater.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace AdvancedUnityPlugin
+{
+    [CreateAssetMenu]
+    public class Updater : GameEvent
+    {
+        public class Looper : MonoBehaviour
+        {
+            private GameEvent onUpdateEvent;
+
+            public void Init(GameEvent onUpdateEvent)
+            {
+                this.onUpdateEvent = onUpdateEvent;
+            }
+
+            private void Update()
+            {
+                if (!onUpdateEvent)
+                    onUpdateEvent.Raise();
+            }
+        }
+
+        [SerializeField]
+        private Looper looper;
+
+        private Looper CreateLooper()
+        {
+            Looper tmp = new GameObject("Looper").AddComponent<Looper>();
+            DontDestroyOnLoad(tmp.gameObject);//TODO : scene load
+            return tmp;
+        }
+
+        public override void RegisterListener(Listener listener)
+        {
+            if (!looper)
+            {
+                looper = CreateLooper();
+                looper.Init(this);
+            }
+
+            base.RegisterListener(listener);
+        }
+    }
+}
+
