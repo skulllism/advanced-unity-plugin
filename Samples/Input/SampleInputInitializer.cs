8adfe12a4402564c0a4a4232148f7c4386a1e52e
skulllism <editer77@gmail.com>
2018-06-13 15:19:19 +0900

    input sample

---
+++ Samples/Input/SampleInputInitializer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using AdvancedUnityPlugin;
+
+public class SampleInputInitializer : MonoBehaviour , AdvancedUnityPlugin.Input.EventListener
+{
+    public AdvancedUnityPlugin.Input input;
+
+    public void OnKeyDown(string keyName)
+    {
+        if (keyName == "SampleA")
+            Debug.Log("Raised SampleA Down Event!");
+        if (keyName == "SampleB")
+            Debug.Log("Raised SampleB Down Event!");
+    }
+
+    public void OnKeyUp(string keyName)
+    {
+        if (keyName == "SampleA")
+            Debug.Log("Raised SampleA Up Event!");
+        if (keyName == "SampleB")
+            Debug.Log("Raised SampleB Up Event!");
+    }
+
+    private void Awake()
+    {
+        input.Init();
+        input.RegisterEventListener(this);
+    }
+
+    private void Update()
+    {
+        if (input.GetKeyDown("SampleA"))
+            Debug.Log("SampleA is down");
+        if (input.GetKeyDown("SampleB"))
+            Debug.Log("SampleB is down");
+    }
+}
