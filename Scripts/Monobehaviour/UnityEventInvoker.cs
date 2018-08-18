f07d945f513210a2bd2f712953024d28fead88f2
skulllism <kimyeongkim@clicked.co.kr>
2018-08-18 23:53:22 +0900

    implement invoker

---
+++ Scripts/Monobehaviour/UnityEventInvoker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace AdvancedUnityPlugin
+{
+    public class UnityEventInvoker : MonoBehaviour
+    {
+        public UnityEvent onInvoke;
+        
+        public void Invoke()
+        {
+            onInvoke.Invoke();
+        }
+    }
+
+}
