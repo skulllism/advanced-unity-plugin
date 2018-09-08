622357f24f2cb444249a6a4780c5220224f30f3d
skulllism <kimyeongkim@clicked.co.kr>
2018-09-08 19:29:15 +0900

    removed scene events

--- Scripts/SceneLoadedUnityEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.Events;
-using UnityEngine.SceneManagement;
-
-namespace AdvancedUnityPlugin
-{
-    [System.Serializable]
-    public class SceneLoadedUnityEvent : UnityEvent<Scene , LoadSceneMode> { }
-}
