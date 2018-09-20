9275991890abce843dd2a18c2a0117ab62724fdd
skulllism <kimyeongkim@clicked.co.kr>
2018-09-20 17:55:45 +0900

    mod path of multi-scene-loader

---
+++ Scripts/Monobehaviour/MultiSceneLoader.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AdvancedUnityPlugin
+{
+    public class MultiSceneLoader : MonoBehaviour
+    {
+        public List<string> requirements;
+
+        public StringStorage prevRequirements;
+
+        public SceneController controller;
+        public Looper looper;
+
+        private void Awake()
+        {
+            SceneManager.activeSceneChanged += OnActiveSceneChanaged;
+        }
+
+        private void OnDestroy()
+        {
+            SceneManager.activeSceneChanged -= OnActiveSceneChanaged;
+        }
+
+        private void OnActiveSceneChanaged(Scene prev, Scene next)
+        {
+            if (next != gameObject.scene)
+                return;
+
+            UnloadRequirements();
+            LoadRequirements();
+        }
+
+        public void LoadRequirements()
+        {
+            prevRequirements.strings = requirements;
+
+            Queue<IEnumerator> additiveLoadRoutines = new Queue<IEnumerator>();
+
+            foreach (var requirement in requirements)
+            {
+                foreach (var loaded in controller.loadedScene)
+                {
+                    if (requirement == loaded.name)
+                        continue;
+                }
+
+
+                additiveLoadRoutines.Enqueue(controller.AdditiveLoad(requirement));
+            }
+
+            looper.monoBehaviour.worker.StartWork("multi_scene_load" , additiveLoadRoutines);
+
+        }
+
+        public void UnloadRequirements()
+        {
+            Queue<IEnumerator> unloadRoutines = new Queue<IEnumerator>();
+
+            foreach (var prevLoaded in prevRequirements.strings)
+            {
+                if (prevLoaded == gameObject.scene.name)
+                    continue;
+
+                foreach (var requirement in requirements)
+                {
+                    if (requirement == prevLoaded)
+                        continue;
+                }
+
+                unloadRoutines.Enqueue(controller.Unload(looper.monoBehaviour , prevLoaded));
+            }
+
+            looper.monoBehaviour.worker.StartWork("multi_scene_unload" , unloadRoutines);
+        }
+    }
+}
