2bf50c744ec4d5afed1e700e6e1b4e785c9a37d1
youlukly <youbadly@naver.com>
2018-09-27 01:17:02 +0900

    add keyValuesPairs Initializer

--- Scripts/KeyValuesPairs.cs
+++ Scripts/KeyValuesPairs.cs
@@ -6,5 +6,11 @@ namespace AdvancedUnityPlugin
     public class KeyValuesPairs
     {
         public KeyValuesPair[] pairs;
+
+        public KeyValuesPairs(KeyValuesPair[] pairs)
+        {
+            this.pairs = pairs;
+        }
+            
     }
 }
