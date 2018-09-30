8f382ba92c495157e1cf6b3f424654faf333d767
skulllism <kimyeongkim@clicked.co.kr>
2018-09-30 15:47:18 +0900

    add update-event-broadcaster

---
+++ Scripts/Monobehaviour/UpdateEventBroadcaster.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace AdvancedUnityPlugin
+{
+    public class UpdateEventBroadcaster : MonoBehaviour
+    {
+        public UnityEvent onUpdate;
+
+        private void Update()
+        {
+            onUpdate.Invoke();
+        }
+    }
+}
