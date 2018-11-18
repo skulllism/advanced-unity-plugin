2e61e1746c0afe74af2a3197ef0969ccd6686ac9
youlukly <youbadly@naver.com>
2018-11-18 17:40:12 +0900

    fix error

--- AdvancedEditor/Editor/AdvancedStateMachineEditor/ASMTransitionModifyView.cs
+++ AdvancedEditor/Editor/AdvancedStateMachineEditor/ASMTransitionModifyView.cs
@@ -1,202 +1,202 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEditor;
 
-namespace AdvancedUnityPlugin.Editor
-{
-    public class ASMTransitionModifyView : ViewBase
-    {
-        public ASMTransitionModifyView() : base("StateModifyView"){}
+//namespace AdvancedUnityPlugin.Editor
+//{
+//    public class ASMTransitionModifyView : ViewBase
+//    {
+//        public ASMTransitionModifyView() : base("StateModifyView"){}
 
-        private SerializedProperty transitionProperty;
-        private AdvancedStateMachine.AdvancedTransition transition;
-        private EditorNode<AdvancedStateMachineEditorWindow.NodeData> node;
+//        private SerializedProperty transitionProperty;
+//        private AdvancedStateMachine.AdvancedTransition transition;
+//        private EditorNode<AdvancedStateMachineEditorWindow.NodeData> node;
 
-        public override void UpdateView(Rect editorRect, Rect percentageRect)
-        {
-            base.UpdateView(editorRect, percentageRect);
-        }
+//        public override void UpdateView(Rect editorRect, Rect percentageRect)
+//        {
+//            base.UpdateView(editorRect, percentageRect);
+//        }
 
-        public override void GUIView(Event e)
-        {
-            base.GUIView(e);
+//        public override void GUIView(Event e)
+//        {
+//            base.GUIView(e);
 
-            ProcessEvents(e);
+//            ProcessEvents(e);
 
-            GUILayout.BeginArea(viewRect, "", "box");
-            {
-                DrawSerializedPropertyInformation();
+//            GUILayout.BeginArea(viewRect, "", "box");
+//            {
+//                DrawSerializedPropertyInformation();
 
-                DrawDecisionButtn();
+//                DrawDecisionButtn();
 
-                DrawSerializedPropertyEvents();
-            }
-            GUILayout.EndArea();
-        }
+//                DrawSerializedPropertyEvents();
+//            }
+//            GUILayout.EndArea();
+//        }
 
-        public override void ProcessEvents(Event e)
-        {
-            base.ProcessEvents(e);
-        }
+//        public override void ProcessEvents(Event e)
+//        {
+//            base.ProcessEvents(e);
+//        }
 
-        public void Initialize(SerializedProperty data, EditorNode<AdvancedStateMachineEditorWindow.NodeData> node)
-        {
-            if (data == null || node == null)
-                return;
+//        public void Initialize(SerializedProperty data, EditorNode<AdvancedStateMachineEditorWindow.NodeData> node)
+//        {
+//            if (data == null || node == null)
+//                return;
 
-            this.node = node;
-            transitionProperty = data;
-            transition = node.myData.transition;
+//            this.node = node;
+//            transitionProperty = data;
+//            transition = node.myData.transition;
 
-            popupList = AdvancedStateMachineEditorWindow.Instance.stateNames;
+//            popupList = AdvancedStateMachineEditorWindow.Instance.stateNames;
 
-            popupIndex = FindTransitionIndex();
-        }
+//            popupIndex = FindTransitionIndex();
+//        }
 
-        private int FindTransitionIndex()
-        {
-            if (transition.stateID == string.Empty)
-                return 0;
+//        private int FindTransitionIndex()
+//        {
+//            if (transition.stateID == string.Empty)
+//                return 0;
             
-            for (int i = 0; i < AdvancedStateMachineEditorWindow.Target.advancedStates.Count; i++)
-            {
-                if(AdvancedStateMachineEditorWindow.Target.advancedStates[i].ID == transition.stateID)
-                {
-                    return i + 1;
-                }
-            }
+//            for (int i = 0; i < AdvancedStateMachineEditorWindow.Target.advancedStates.Count; i++)
+//            {
+//                if(AdvancedStateMachineEditorWindow.Target.advancedStates[i].ID == transition.stateID)
+//                {
+//                    return i + 1;
+//                }
+//            }
 
-            return 0;
-        }
+//            return 0;
+//        }
 
-        private void DrawSerializedPropertyInformation()
-        {
-            if (transitionProperty == null)
-                return;
+//        private void DrawSerializedPropertyInformation()
+//        {
+//            if (transitionProperty == null)
+//                return;
 
-            transitionProperty.serializedObject.Update();
+//            transitionProperty.serializedObject.Update();
 
-            GUILayout.BeginVertical("box");
-            {
-                GUILayout.BeginHorizontal();
-                {
-                    GUILayout.Label("Transition ID : ");
+//            GUILayout.BeginVertical("box");
+//            {
+//                GUILayout.BeginHorizontal();
+//                {
+//                    GUILayout.Label("Transition ID : ");
    
-                    EditorGUI.BeginChangeCheck();
-                    {
-                        node.myData.transition.ID  = GUILayout.TextField(node.myData.transition.ID);
-                        node.title = node.myData.transition.ID;
+//                    EditorGUI.BeginChangeCheck();
+//                    {
+//                        node.myData.transition.ID  = GUILayout.TextField(node.myData.transition.ID);
+//                        node.title = node.myData.transition.ID;
 
-                    }
-                    if(EditorGUI.EndChangeCheck())
-                    {
-                        AdvancedStateMachineEditorWindow.Instance.InitializePropertyData();    
-                    }
-                }
-                GUILayout.EndHorizontal();
+//                    }
+//                    if(EditorGUI.EndChangeCheck())
+//                    {
+//                        AdvancedStateMachineEditorWindow.Instance.InitializePropertyData();    
+//                    }
+//                }
+//                GUILayout.EndHorizontal();
 
-                GUILayout.BeginHorizontal("box");
-                {
-                    GUILayout.Label("Target State : ");
-                    DrawTransitionList();
-                }
-                GUILayout.EndHorizontal();
-            }
-            GUILayout.EndVertical();  
+//                GUILayout.BeginHorizontal("box");
+//                {
+//                    GUILayout.Label("Target State : ");
+//                    DrawTransitionList();
+//                }
+//                GUILayout.EndHorizontal();
+//            }
+//            GUILayout.EndVertical();  
 
-            transitionProperty.serializedObject.ApplyModifiedProperties();
-        }
+//            transitionProperty.serializedObject.ApplyModifiedProperties();
+//        }
 
-        private int popupIndex = 0;
-        private string[] popupList = new string[15];
-        private void DrawTransitionList()
-        {
-            transitionProperty.serializedObject.Update();
+//        private int popupIndex = 0;
+//        private string[] popupList = new string[15];
+//        private void DrawTransitionList()
+//        {
+//            transitionProperty.serializedObject.Update();
 
-            EditorGUI.BeginChangeCheck();
-            {
-                popupIndex = EditorGUILayout.Popup(popupIndex, popupList);    
-            }
-            if(EditorGUI.EndChangeCheck())
-            {
-                EditorNode<AdvancedStateMachineEditorWindow.NodeData> stateNode = AdvancedStateMachineEditorWindow.Instance.FindNodeByStateID(transition.stateID);
-                if(stateNode != null)
-                    stateNode.RemoveParentNode(node);
+//            EditorGUI.BeginChangeCheck();
+//            {
+//                popupIndex = EditorGUILayout.Popup(popupIndex, popupList);    
+//            }
+//            if(EditorGUI.EndChangeCheck())
+//            {
+//                EditorNode<AdvancedStateMachineEditorWindow.NodeData> stateNode = AdvancedStateMachineEditorWindow.Instance.FindNodeByStateID(transition.stateID);
+//                if(stateNode != null)
+//                    stateNode.RemoveParentNode(node);
                 
-                node.RemoveChildNode(stateNode);
+//                node.RemoveChildNode(stateNode);
                        
-                if (popupIndex > 0)
-                {
-                    node.myData.transition.stateID = AdvancedStateMachineEditorWindow.Target.advancedStates[popupIndex - 1].ID;
+//                if (popupIndex > 0)
+//                {
+//                    node.myData.transition.stateID = AdvancedStateMachineEditorWindow.Target.advancedStates[popupIndex - 1].ID;
 
-                    AdvancedStateMachineEditorWindow.Instance.AttachChildNodeInParentNode(AdvancedStateMachineEditorWindow.Instance.FindNodeByStateID(transition.stateID), node);
-                }
-                else
-                {
-                    node.myData.transition.stateID = string.Empty;
-                }
+//                    AdvancedStateMachineEditorWindow.Instance.AttachChildNodeInParentNode(AdvancedStateMachineEditorWindow.Instance.FindNodeByStateID(transition.stateID), node);
+//                }
+//                else
+//                {
+//                    node.myData.transition.stateID = string.Empty;
+//                }
                                                                                                    
-                AdvancedStateMachineEditorWindow.Instance.InitializePropertyData();
-            }
+//                AdvancedStateMachineEditorWindow.Instance.InitializePropertyData();
+//            }
 
-            transitionProperty.serializedObject.ApplyModifiedProperties();
-        }
+//            transitionProperty.serializedObject.ApplyModifiedProperties();
+//        }
 
-        private bool isCreate;
-        private bool isDelete;
-        private void DrawDecisionButtn()
-        {
-            GUILayout.BeginHorizontal();
-            {
-                isCreate = GUILayout.Button("+");
-                isDelete = GUILayout.Button("-");
+//        private bool isCreate;
+//        private bool isDelete;
+//        private void DrawDecisionButtn()
+//        {
+//            GUILayout.BeginHorizontal();
+//            {
+//                isCreate = GUILayout.Button("+");
+//                isDelete = GUILayout.Button("-");
 
-                if(isCreate)
-                {
-                    transition.decisions.Add(new AdvancedStateMachine.AdvancedDecision());
-                }
+//                if(isCreate)
+//                {
+//                    transition.decisions.Add(new AdvancedStateMachine.AdvancedDecision());
+//                }
 
-                if(isDelete)
-                {
-                    transition.decisions.RemoveAt(transition.decisions.Count - 1);
-                }
-            }
-            GUILayout.EndHorizontal();
-        }
+//                if(isDelete)
+//                {
+//                    transition.decisions.RemoveAt(transition.decisions.Count - 1);
+//                }
+//            }
+//            GUILayout.EndHorizontal();
+//        }
 
-        private SerializedObject decision;
-        private Vector2 propertyScroll = Vector2.zero;
-        private void DrawSerializedPropertyEvents()
-        {
-            if (transitionProperty == null)
-                return;
+//        private SerializedObject decision;
+//        private Vector2 propertyScroll = Vector2.zero;
+//        private void DrawSerializedPropertyEvents()
+//        {
+//            if (transitionProperty == null)
+//                return;
             
-            GUILayout.BeginHorizontal((GUIStyle)"AnimationKeyframeBackground");
-            {
-                propertyScroll = GUILayout.BeginScrollView(propertyScroll, false, false);
-                {
-                    transitionProperty.serializedObject.Update();
+//            GUILayout.BeginHorizontal((GUIStyle)"AnimationKeyframeBackground");
+//            {
+//                propertyScroll = GUILayout.BeginScrollView(propertyScroll, false, false);
+//                {
+//                    transitionProperty.serializedObject.Update();
 
-                    if (EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions")))
-                    {
-                        for (int i = 0; i < transitionProperty.FindPropertyRelative("decisions").arraySize; i++)
-                        {
-                            GUILayout.BeginVertical("box");
-                            {
-                                EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions").GetArrayElementAtIndex(i).FindPropertyRelative("isTrue"), new GUIContent("isTrue"));
-                                EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions").GetArrayElementAtIndex(i).FindPropertyRelative("condition"), new GUIContent("Condition"));
-                            }
-                            GUILayout.EndVertical();
-                        }
-                    }
+//                    if (EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions")))
+//                    {
+//                        for (int i = 0; i < transitionProperty.FindPropertyRelative("decisions").arraySize; i++)
+//                        {
+//                            GUILayout.BeginVertical("box");
+//                            {
+//                                EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions").GetArrayElementAtIndex(i).FindPropertyRelative("isTrue"), new GUIContent("isTrue"));
+//                                EditorGUILayout.PropertyField(transitionProperty.FindPropertyRelative("decisions").GetArrayElementAtIndex(i).FindPropertyRelative("condition"), new GUIContent("Condition"));
+//                            }
+//                            GUILayout.EndVertical();
+//                        }
+//                    }
 
-                    transitionProperty.serializedObject.ApplyModifiedProperties();
-                }
-                GUILayout.EndScrollView();
-            }
-            GUILayout.EndHorizontal();
-        }
-    }
-}
+//                    transitionProperty.serializedObject.ApplyModifiedProperties();
+//                }
+//                GUILayout.EndScrollView();
+//            }
+//            GUILayout.EndHorizontal();
+//        }
+//    }
+//}
 
--- AdvancedEditor/Editor/AdvancedStateMachineEditor/AdvancedStateMachineEditorWindow.Finder.cs
+++ AdvancedEditor/Editor/AdvancedStateMachineEditor/AdvancedStateMachineEditorWindow.Finder.cs
@@ -1,111 +1,111 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-namespace AdvancedUnityPlugin.Editor
-{
-    public partial class AdvancedStateMachineEditorWindow 
-    {
-        public int FindStateIndexByNode(EditorNode<NodeData> node)
-        {
-            if (node == null)
-                return -1;
+//namespace AdvancedUnityPlugin.Editor
+//{
+//    public partial class AdvancedStateMachineEditorWindow 
+//    {
+//        public int FindStateIndexByNode(EditorNode<NodeData> node)
+//        {
+//            if (node == null)
+//                return -1;
             
-            for (int i = 0; i < selected.advancedStates.Count; i++)
-            {
-                if (selected.advancedStates[i].ID == node.myData.state.ID)
-                {
-                    return i;
-                }
-            }
+//            for (int i = 0; i < selected.advancedStates.Count; i++)
+//            {
+//                if (selected.advancedStates[i].ID == node.myData.state.ID)
+//                {
+//                    return i;
+//                }
+//            }
 
-            return -1;
-        }
+//            return -1;
+//        }
 
-        public int FindTransitionIndexByNode(EditorNode<NodeData> node)
-        {
-            if (node == null)
-                return -1;
+//        public int FindTransitionIndexByNode(EditorNode<NodeData> node)
+//        {
+//            if (node == null)
+//                return -1;
             
-            for (int i = 0; i < selected.advancedTransitions.Count; i++)
-            {
-                if (selected.advancedTransitions[i].ID == node.myData.transition.ID)
-                {
-                    return i;
-                }
-            }
-            return -1;
-        }
+//            for (int i = 0; i < selected.advancedTransitions.Count; i++)
+//            {
+//                if (selected.advancedTransitions[i].ID == node.myData.transition.ID)
+//                {
+//                    return i;
+//                }
+//            }
+//            return -1;
+//        }
 
-        public AdvancedStateMachine.AdvancedTransition FindTransitionByNode(EditorNode<NodeData> node)
-        {
-            if (node == null)
-                return null;
+//        public AdvancedStateMachine.AdvancedTransition FindTransitionByNode(EditorNode<NodeData> node)
+//        {
+//            if (node == null)
+//                return null;
             
-            for (int i = 0; i < selected.advancedTransitions.Count; i++)
-            {
-                if (selected.advancedTransitions[i].ID == node.myData.transition.ID)
-                {
-                    return selected.advancedTransitions[i];
-                }
-            }
-            return null;
-        }
+//            for (int i = 0; i < selected.advancedTransitions.Count; i++)
+//            {
+//                if (selected.advancedTransitions[i].ID == node.myData.transition.ID)
+//                {
+//                    return selected.advancedTransitions[i];
+//                }
+//            }
+//            return null;
+//        }
 
-        public EditorNode<NodeData> FindNodeByTransition(AdvancedStateMachine.AdvancedTransition transition)
-        {
-            if (transition == null)
-                return null;
+//        public EditorNode<NodeData> FindNodeByTransition(AdvancedStateMachine.AdvancedTransition transition)
+//        {
+//            if (transition == null)
+//                return null;
 
-            for (int i = 0; i < editorNodes.Count; i++)
-            {
-                if (editorNodes[i].myData.type != NodeType.TRANSITION)
-                    continue;
+//            for (int i = 0; i < editorNodes.Count; i++)
+//            {
+//                if (editorNodes[i].myData.type != NodeType.TRANSITION)
+//                    continue;
 
-                if (editorNodes[i].myData.transition.ID == transition.ID)
-                    return editorNodes[i];
-            }
+//                if (editorNodes[i].myData.transition.ID == transition.ID)
+//                    return editorNodes[i];
+//            }
 
-            return null;
-        }
+//            return null;
+//        }
 
-        public EditorNode<NodeData> FindNodeByState(AdvancedStateMachine.AdvancedState state)
-        {
-            if (state == null)
-                return null;
+//        public EditorNode<NodeData> FindNodeByState(AdvancedStateMachine.AdvancedState state)
+//        {
+//            if (state == null)
+//                return null;
 
-            if (state.ID == null)
-                return null;
+//            if (state.ID == null)
+//                return null;
 
-            for (int i = 0; i < editorNodes.Count; i++)
-            {
-                if (editorNodes[i].myData.type != NodeType.STATE)
-                    continue;
+//            for (int i = 0; i < editorNodes.Count; i++)
+//            {
+//                if (editorNodes[i].myData.type != NodeType.STATE)
+//                    continue;
 
-                if (editorNodes[i].myData.state.ID == state.ID)
-                    return editorNodes[i];
-            }
+//                if (editorNodes[i].myData.state.ID == state.ID)
+//                    return editorNodes[i];
+//            }
 
-            return null;
-        }
+//            return null;
+//        }
 
-        public EditorNode<NodeData> FindNodeByStateID(string stateID)
-        {
-            if (stateID == string.Empty)
-                return null;
+//        public EditorNode<NodeData> FindNodeByStateID(string stateID)
+//        {
+//            if (stateID == string.Empty)
+//                return null;
 
-            for (int i = 0; i < editorNodes.Count; i++)
-            {
-                if (editorNodes[i].myData.type != NodeType.STATE)
-                    continue;
+//            for (int i = 0; i < editorNodes.Count; i++)
+//            {
+//                if (editorNodes[i].myData.type != NodeType.STATE)
+//                    continue;
 
-                if (editorNodes[i].myData.state.ID == stateID)
-                    return editorNodes[i];
-            }
+//                if (editorNodes[i].myData.state.ID == stateID)
+//                    return editorNodes[i];
+//            }
 
-            return null;
-        }
-    }
-}
+//            return null;
+//        }
+//    }
+//}
 
 
