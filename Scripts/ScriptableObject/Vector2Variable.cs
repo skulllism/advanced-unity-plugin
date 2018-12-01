7aa2f777047e551ceec80aeaedcb04a9e39548e2
youlukly <youbadly@naver.com>
2018-12-01 17:22:48 +0900

    add Vector2-variable

---
+++ Scripts/ScriptableObject/Vector2Variable.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+namespace AdvancedUnityPlugin
+{
+    [CreateAssetMenu(menuName = "AdvancedUnityPlugin/Data/Vector2")]
+    public class Vector2Variable : ScriptableObject, ISerializationCallbackReceiver
+    {
+        public Vector2 initialValue;
+
+        public Vector2 runtimeValue { set; get; }
+
+        public void OnAfterDeserialize()
+        {
+            runtimeValue = initialValue;
+        }
+
+        public void OnBeforeSerialize() { }
+    }
+}
