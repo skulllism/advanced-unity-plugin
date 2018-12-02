720881074c76cced3edfacae78e1d4371e6b70e2
skulllism <kimyeongkim@clicked.co.kr>
2018-12-03 00:58:21 +0900

    added ObjectsTransferTrigger

---
+++ Scripts/Monobehaviour/ObjectsTransferTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+
+namespace AdvancedUnityPlugin
+{
+    [Serializable]
+    public class ObjectsUnityEvent : UnityEvent<List<object>> { }
+
+    public class ObjectsTransferTrigger : MonoBehaviour
+    {
+        [SerializeField]
+        public ObjectsUnityEvent onHit;
+        public readonly List<object> objects = new List<object>();
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            ObjectsTransferTrigger hit = collision.gameObject.GetComponentInChildren<ObjectsTransferTrigger>();
+
+            if (!hit)
+                return;
+
+            hit.onHit.Invoke(objects);
+        }
+    }
+}
