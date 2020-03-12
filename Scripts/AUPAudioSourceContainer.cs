408077d8a9730e9f3af031ad46d29f260c4d31c1
skulllism <editer77@gmail.com>
2020-03-13 03:00:54 +0900

    Add AUPPhysics2D and AudioSourceContainer

---
+++ Scripts/AUPAudioSourceContainer.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AUPAudioSourceContainer
+{
+    private const int max = 2;
+    private readonly Dictionary<string, AudioSourcePool> sources = new Dictionary<string, AudioSourcePool>();
+
+    public class AudioSourcePool
+    {
+        public AudioSource origin { get; private set; }
+        public int count { get; private set; }
+
+        private Transform parent;
+
+        protected List<AudioSource> pools = new List<AudioSource>();
+
+        public void Init(AudioSource origin, int count, Transform parent)
+        {
+            this.origin = origin;
+            this.count = count;
+            this.parent = parent;
+
+            Init();
+        }
+
+        private void Init()
+        {
+            Debug.Assert(origin);
+            Debug.Assert(count > 0);
+
+            for (int i = 0; i < count; i++)
+            {
+                Create();
+            }
+        }
+
+        private AudioSource Create()
+        {
+            AudioSource component = GameObject.Instantiate(origin, parent, false);
+            pools.Add(component);
+            return component;
+        }
+
+        public AudioSource Get()
+        {
+            foreach (var pool in pools)
+            {
+                if (pool.isPlaying)
+                    continue;
+
+                return pool;
+            }
+
+            //Debug.Log("Create on demand : " + origin.name);
+            Create();
+
+            return Get();
+        }
+
+        public void PoolAll()
+        {
+            foreach (var pool in pools)
+            {
+                pool.Stop();
+            }
+        }
+
+        public void Reset()
+        {
+            DestroyAll();
+            Init();
+        }
+
+        public void DestroyAll()
+        {
+            foreach (var pool in pools)
+            {
+                GameObject.Destroy(pool.gameObject);
+            }
+
+            pools.Clear();
+        }
+    }
+    private const string sourcePath = "Audio/Sources/";
+
+    public AUPAudioSourceContainer(Transform parent)
+    {
+        LoadSources(parent);
+    }
+
+    private void LoadSources(Transform parent)
+    {
+        AudioSource[] array = Resources.LoadAll<AudioSource>(sourcePath);
+
+        foreach (var source in array)
+        {
+            var pool = new AudioSourcePool();
+            pool.Init(source, max, parent);
+
+            sources.Add(source.name, pool);
+        }
+        //Debug.Log(sources.Count);
+    }
+
+    public AudioSource Get(string sourceType)
+    {
+        return sources.ContainsKey(sourceType) ? sources[sourceType].Get() : null;
+    }
+
+}
---
+++ Scripts/AUPPhysics2D.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AUPPhysics2D
+{
+    public Rigidbody2D Rigidbody2D { private set; get; }
+    public AUPRigidbody2DUtility Rigidbody2DController { private set; get; }
+    public AUPMove2D Move2D { private set; get; }
+    public AUPForward Forward { private set; get; }
+    public Collider2D[] Colliders { get; private set; }
+
+    private float defaultDrag;
+
+    public AUPPhysics2D(Rigidbody2D rigidbody2D, Transform transform, string motionColliderPath, float defaultDrag, eHorizontalDirection startDirection)
+    {
+        this.defaultDrag = defaultDrag;
+        this.Rigidbody2D = rigidbody2D;
+        Forward = new AUPForward();
+        Forward.Init(transform, startDirection);
+
+        Rigidbody2DController = new AUPRigidbody2DUtility(rigidbody2D, Forward);
+
+        Colliders = GameObject.Instantiate(Resources.Load<GameObject>(motionColliderPath), transform, false).GetComponents<Collider2D>();
+
+        Move2D = new AUPMove2D(rigidbody2D, Colliders[0]);
+    }
+
+    public void SetDefaultDrag()
+    {
+        Rigidbody2D.drag = defaultDrag;
+    }
+}
