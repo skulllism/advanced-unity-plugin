7a4002a50801297a6ad0658e0fab0513ceffe6d7
skulllism <kimyeongkim@clicked.co.kr>
2020-04-29 20:08:38 +0900

    Remove Forward in Physics

--- Scripts/AUPPhysics2D.cs
+++ Scripts/AUPPhysics2D.cs
@@ -5,16 +5,13 @@ public class AUPPhysics2D
     public Rigidbody2D Rigidbody2D { private set; get; }
     public AUPRigidbody2DUtility Rigidbody2DController { private set; get; }
     public AUPMove2D Move2D { private set; get; }
-    public AUPForward Forward { private set; get; }
     public Collider2D[] Colliders { get; private set; }
     
-    public AUPPhysics2D(Rigidbody2D rigidbody2D, Transform transform, string motionColliderPath, HorizontalDirection startDirection)
+    public AUPPhysics2D(Rigidbody2D rigidbody2D, Transform transform, string motionColliderPath)
     {
         Rigidbody2D = rigidbody2D;
-        Forward = new AUPForward();
-        Forward.Init(transform, startDirection);
 
-        Rigidbody2DController = new AUPRigidbody2DUtility(rigidbody2D, Forward);
+        Rigidbody2DController = new AUPRigidbody2DUtility(rigidbody2D);
 
         Colliders = Object.Instantiate(Resources.Load<GameObject>(motionColliderPath), transform, false).GetComponents<Collider2D>();
 
