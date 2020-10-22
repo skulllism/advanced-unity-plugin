524390184ea154f9ed61f8dddd7a36475fec01fa
YJT <dbwhdxo789@gmail.com>
2020-10-23 00:57:33 +0900

    Refactor Pool

---
+++ Scripts/MonobehaviorPool.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using VaporWorld;
+
+namespace AdvancedUnityPlugin
+{
+    public class MonobehaviorPool : MonoBehaviour
+    {
+        private Pool pool;
+        private bool isInitailized;
+
+        public GameObject origin;
+        public int count;
+
+        public void Init(GameObject origin, int count)
+		{
+            if (isInitailized)
+            {
+                return;
+            }
+
+            isInitailized = true;
+            if (!origin || count <= 0)
+                return;
+
+            pool = new Pool(origin, count);
+        }
+
+        private void Awake()
+		{
+            Init(origin, count);
+        }
+
+        private PoolableObject Create()
+        {
+            PoolableObject gameObject = pool.Get();
+            return gameObject;
+        }
+
+        public PoolableObject Get()
+        {
+            return pool.Get();
+        }
+
+        public PoolableObject Get(Vector3 position)
+        {
+            PoolableObject gameObject = Get();
+            gameObject.transform.position = position;
+
+            return gameObject;
+        }
+
+        public PoolableObject Get(Transform parent, bool worldPositionStays = false)
+        {
+            PoolableObject gameObject = Get();
+            gameObject.transform.SetParent(parent, worldPositionStays);
+            return gameObject;
+        }
+
+        public void Create(Transform parent)
+        {
+            Get(parent, false);
+        }
+
+        public void PoolAll()
+        {
+            pool.PoolAll();
+        }
+
+        public void DestroyAll()
+        {
+            pool.DestroyAll();
+        }
+    }
+
+    public class Pool
+    {
+        public GameObject origin;
+        public int count;
+
+        protected List<PoolableObject> pools = new List<PoolableObject>();
+        private Transform parent; 
+
+		public Pool(GameObject origin, int count)
+		{
+			this.origin = origin;
+			this.count = count;
+
+            Debug.Assert(origin);
+            Debug.Assert(count > 0);
+            parent = new GameObject("Pool_" + origin.name).transform;
+            for (int i = 0; i < count; i++)
+            {
+                Create(parent);
+            }
+        }
+
+        private PoolableObject Create(Transform parent=null)
+        {
+            PoolableObject component = GameObject.Instantiate(origin).AddComponent<PoolableObject>();
+            component.name = origin.name;
+            component.transform.SetParent(parent);
+            component.Initialize(this);
+            component.gameObject.SetActive(false);
+            pools.Add(component);
+            return component;
+        }
+        public void Reset(PoolableObject poolableObject)
+		{
+            //예외처리
+            if (poolableObject.name != origin.name)
+            {
+                return;
+            }
+            //부모 리셋
+            poolableObject.transform.SetParent(parent);
+            //비활성
+            if (!poolableObject.gameObject.activeSelf)
+            {
+                poolableObject.gameObject.SetActive(false);
+            }
+        }
+        public void Remove(PoolableObject poolableObject)
+		{
+            pools.Remove(poolableObject);
+        }
+
+        public PoolableObject Get(Transform parent = null)
+        {
+            foreach (var pool in pools)
+            {
+                if (pool.gameObject.activeSelf)
+                    continue;
+
+                pool.gameObject.SetActive(true);
+
+                return pool;
+            }
+
+            Debug.Log("Create on demand : " + origin.name);
+            Create(parent);
+
+            return Get();
+        }
+
+        public void PoolAll()
+        {
+            foreach (var pool in pools)
+            {
+                pool.gameObject.SetActive(false);
+            }
+        }
+
+        //public void Reset()
+        //{
+        //    DestroyAll();
+        //    Init();
+        //}
+
+        public void DestroyAll()
+        {
+            foreach (var pool in pools)
+            {
+                GameObject.Destroy(pool.gameObject);
+            }
+
+            pools.Clear();
+        }
+    }
+
+}
