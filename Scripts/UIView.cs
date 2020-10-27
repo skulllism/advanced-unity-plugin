396c79f4b0bc19c7d66ecaeaacd45dd3368676d5
skulllism <kimyeongkim@clicked.co.kr>
2020-10-28 02:44:05 +0900

    Add GameManager event handler in UIView

--- Scripts/UIView.cs
+++ Scripts/UIView.cs
@@ -4,20 +4,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using VaporWorld;
 
-public class UIView : MonoBehaviour
+public class UIView : VaporWorldBehaviour
 {
     public bool isAlwaysShow;
     public Graphic firstSelect;
 
     private static List<UIView> views = new List<UIView>();
 
-    protected virtual void Awake()
+    public override void OnPlayerInitialized(Player player)
     {
         transform.localPosition = Vector3.zero;
         views.Add(this);
         gameObject.SetActive(false);
-
     }
 
 	private void OnDestroy()
@@ -83,5 +83,9 @@ public class UIView : MonoBehaviour
     public void Hide()
     {
         HideImmediately();
+    }
+
+    public override void OnGameManagerInitialized(GameManager gameManager)
+    {
     }
 }
