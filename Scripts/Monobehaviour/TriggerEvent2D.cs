f437bd3ded44545126eedac23edadc6c5ca5accf
Knam <rlska7879@naver.com>
2022-02-15 21:45:14 +0900

    Fix TriggerEvent2D

--- Scripts/Monobehaviour/TriggerEvent2D.cs
+++ Scripts/Monobehaviour/TriggerEvent2D.cs
@@ -5,22 +5,35 @@ namespace AdvancedUnityPlugin
     [RequireComponent(typeof(Collider2D))]
     public class TriggerEvent2D : MonoBehaviour
     {
+        [SerializeField] private string tagName;
         public Collider2DUnityEvent onTriggerEnter;
         public Collider2DUnityEvent onTriggerStay;
         public Collider2DUnityEvent onTriggerExit;
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            if (!this.gameObject.CompareTag(this.tagName))
+            {
+                return;
+            }
             onTriggerEnter.Invoke(other);
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
+            if (!this.gameObject.CompareTag(this.tagName))
+            {
+                return;
+            }
             onTriggerExit.Invoke(other);
         }
 
         private void OnTriggerStay2D(Collider2D other)
         {
+            if (!this.gameObject.CompareTag(this.tagName))
+            {
+                return;
+            }
             onTriggerStay.Invoke(other);
         }
     }
